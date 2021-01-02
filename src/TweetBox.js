998171845ae8f1817bea6b885296bea6fb9f4cc9
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-02 21:04:24 +0300

    imported button, avatar; create tweet-box css

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
 
 function TweetBox() {
-  return <div></div>;
+  return (
+    <div className="tweet-box">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="" />
+        </div>
+        <Button>Tweet</Button>
+      </form>
+    </div>
+  );
 }
 
 export default TweetBox;
