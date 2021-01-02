9e51a19acd64f715897ae47c77fda578feb1bbb6
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-02 20:40:55 +0300

    created wigets js and css files

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import "./App.css";
 import "./Sidebar.js";
 import Sidebar from "./Sidebar.js";
 import Feed from "./Feed.js";
+import Widgets from "./Widgets.js";
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
-
-      {/* Feed */}
       <Feed />
-
-      {/* Widgets */}
+      <Widgets />
     </div>
   );
 }
--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Feed.css";
 
 function Feed() {
   return (
     <div>
-      {/* Header */}
-      <h2>Home</h2>
+      <div className="feed">
+        <h2>Home</h2>
+      </div>
 
       {/* Tweet Box */}
 
