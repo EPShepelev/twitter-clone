604df57aef8d102e44d35dbe1ecca2a3e834e0ce
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-03 11:08:29 +0300

    created input for image url

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -4,13 +4,18 @@ import { Avatar, Button } from "@material-ui/core";
 
 function TweetBox() {
   return (
-    <div className="tweet-box">
+    <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
           <Avatar src="https://vjoy.cc/wp-content/uploads/2020/10/5bdaa2f76dee907eb45e3e446b50ce4e.jpg" />
-          <input placeholder="what's happening?"></input>
+          <input placeholder="what's happening?" type="text"></input>
         </div>
-        <Button>Tweet</Button>
+        <input
+          className="tweetBox__imageInput"
+          placeholder="Enter image URL (optional)"
+          type="text"
+        ></input>
+        <Button className="tweetBox__tweetButton">Tweet</Button>
       </form>
     </div>
   );
