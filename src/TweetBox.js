49c223d9e6702875266c2cd9ff13d57d33ff1e1f
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-27 14:22:17 +0300

    created send tweet function

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 import "./TweetBox.css";
 import { Avatar, Button } from "@material-ui/core";
+import db from "./firebase";
 
 function TweetBox() {
   const [tweetMessage, setTweeMessage] = useState("");
   const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+    db.collection("posts").add({
+      displayName: "Evgenius",
+      userName: "cleverJenya",
+      verified: true,
+      text: tweetMessage,
+      avatar="https://vjoy.cc/wp-content/uploads/2020/10/5bdaa2f76dee907eb45e3e446b50ce4e.jpg",
+      image: tweetImage,
+
+    })
+  }
 
   return (
     <div className="tweetBox">
@@ -25,7 +39,9 @@ function TweetBox() {
           placeholder="Enter image URL (optional)"
           type="text"
         ></input>
-        <Button className="tweetBox__tweetButton">Tweet</Button>
+        <Button onClick={sendTweet} type="submit" className="tweetBox__tweetButton">
+          Tweet
+        </Button>
       </form>
     </div>
   );
